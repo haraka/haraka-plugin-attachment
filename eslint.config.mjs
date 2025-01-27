8ec3404b9a6374d013da5579022830642629fe35
Matt Simerson <matt@tnpi.net>
2025-01-26 19:50:20 -0800

    chore: format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
-});
+})
 
 export default [
   ...compat.extends('@haraka'),
@@ -22,7 +22,7 @@ export default [
       },
     },
     rules: {
-      'no-unused-vars': ['warn', { 'caughtErrorsIgnorePattern': '^ignore' }]
-    }
+      'no-unused-vars': ['warn', { caughtErrorsIgnorePattern: '^ignore' }],
+    },
   },
-];
+]
